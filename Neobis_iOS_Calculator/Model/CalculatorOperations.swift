fcc2e4a6eec97c252e593302932a4768fa60deda
Asel <mirbekovaasel663@gmail.com>
2023-11-19 19:35:59 +0600

    Initial Commit

---
+++ Neobis_iOS_Calculator/Model/CalculatorOperations.swift
@@ -0,0 +1,22 @@
+
+import Foundation
+
+enum CalculatorOperation {
+    case divide
+    case multiply
+    case subtract
+    case add
+    
+    var title: String {
+        switch self {
+        case .divide:
+            return "÷"
+        case .multiply:
+            return "x"
+        case .subtract:
+            return "-"
+        case .add:
+            return "+"
+        }
+    }
+}
